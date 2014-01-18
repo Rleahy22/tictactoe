3146c78402323d6a3b97077bf458af5a3a7c35f9
Ryan Leahy <ryanleahy90@gmail.com>
2014-01-18 16:37:07 -0600

    created instance variables for cpu spec

--- spec/cpu_spec.rb
+++ spec/cpu_spec.rb
@@ -5,6 +5,8 @@ describe "Cpu" do
   before do
     @game = Game.new
     @cpu = @game.cpu_player
+    @user = @game.user_player
+    @board= @game.board
   end
 
   describe '#initialize' do
@@ -17,15 +19,15 @@ describe "Cpu" do
     end
 
     it "should give the computer access to the game board" do
-      expect(@cpu.board).to be_an_instance_of(Board)
+      expect(@board).to be_an_instance_of(Board)
     end
   end
 
   describe '#is_immediate_threat' do
     describe "when there are 2 X's and no O's on a winning combo" do
       before do
-        @game.user_player.place_mark(1)
-        @game.user_player.place_mark(2)
+        @user.place_mark(1)
+        @user.place_mark(2)
       end
       it "should return true" do
         expect(@cpu.is_immediate_threat?).to eq(true)
@@ -41,9 +43,9 @@ describe "Cpu" do
 
   describe '#set_response_to_threat' do
     before do
-      @game.user_player.place_mark(1)
-      @game.user_player.place_mark(2)
-      squares = @game.board.winning_squares.first.map { |square| square.value}
+      @user.place_mark(1)
+      @user.place_mark(2)
+      squares = @board.winning_squares.first.map { |square| square.value}
       @cpu.set_response_to_threat(squares)
     end
     it "should return the winning move" do
@@ -73,7 +75,7 @@ describe "Cpu" do
     before do
       @cpu.place_mark(1)
       @cpu.place_mark(2)
-      squares = @game.board.winning_squares.first.map { |square| square.value}
+      squares = @board.winning_squares.first.map { |square| square.value}
       @cpu.set_winning_move(squares)
     end
     it "should return the winning move" do
@@ -89,7 +91,7 @@ describe "Cpu" do
     end
 
     describe "when there is a user mark" do
-      before { @game.user_player.place_mark(1) }
+      before { @user.place_mark(1) }
       it "should return true" do
         expect(@cpu.has_user_moved?).to eq(true)
       end
@@ -104,7 +106,7 @@ describe "Cpu" do
     end
 
     describe "when the middle square is occupied" do
-      before { @game.user_player.place_mark(5) }
+      before { @user.place_mark(5) }
       it "should return false" do
         expect(@cpu.is_middle_available?).to eq(false)
       end
@@ -115,14 +117,14 @@ describe "Cpu" do
     describe "when there isn't an immediate opportunity to win" do
       describe "when the player has made the first move" do
         describe "when the player has taken the middle square" do
-          before { @game.user_player.place_mark(5) }
+          before { @user.place_mark(5) }
           it "should return one of the remaining squares" do
             expect([1,2,3,4,6,7,8,9]).to include(@cpu.best_move)
           end
         end
 
         describe "when the player has not taken the middle square" do
-          before { @game.user_player.place_mark(1) }
+          before { @user.place_mark(1) }
           it "should return the middle square" do
             expect(@cpu.best_move).to eq(5)
           end
@@ -132,9 +134,9 @@ describe "Cpu" do
       describe "when both the player and computer have moved already" do
         describe "when there is no immediate threat" do
           before do
-            @game.user_player.place_mark(2)
+            @user.place_mark(2)
             @cpu.place_mark(5)
-            @game.user_player.place_mark(4)
+            @user.place_mark(4)
           end
           it "should return one of the remaining squares" do
               expect([1,3,6,7,8,9]).to include(@cpu.best_move)
@@ -143,9 +145,9 @@ describe "Cpu" do
 
         describe "when there is an immediate threat" do
           before do
-            @game.user_player.place_mark(1)
+            @user.place_mark(1)
             @cpu.place_mark(5)
-            @game.user_player.place_mark(3)
+            @user.place_mark(3)
           end
           it "should return the square that ends the threat" do
             expect(@cpu.best_move).to eq(2)
@@ -157,9 +159,9 @@ describe "Cpu" do
     describe "when there is an immediate opportunity to win" do
       before do
         @cpu.place_mark(1)
-        @game.user_player.place_mark(4)
+        @user.place_mark(4)
         @cpu.place_mark(5)
-        @game.user_player.place_mark(3)
+        @user.place_mark(3)
       end
       it "should return the square that wins the game" do
         expect(@cpu.best_move).to eq(9)
