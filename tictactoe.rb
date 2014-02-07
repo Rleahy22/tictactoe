5c149c84ab2eeb94db9480dbc21f88e899ac6dc7
Ryan Leahy <ryanleahy90@gmail.com>
2014-02-07 11:00:16 -0600

    removed need for user input to begin a game

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,4 @@
 require_relative './lib/game'
 
-puts "Would you like to begin a game? (y/n)"
-response = gets.chomp
-until response.upcase == "Y" || response.upcase == "N"
-  puts "Please enter either y or n"
-  response = gets.chomp
-end
-if response.upcase == "Y"
-  @game = Game.new
-  @game.play
-else
-  puts "Then why are you here?"
-end
+@game = Game.new
+@game.play
